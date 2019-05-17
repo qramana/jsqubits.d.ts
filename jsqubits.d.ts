007014eddcafd6cdb98740a9d1f82f57b9e7d305
kamakiri01 <kamakiri01@users.noreply.github.com>
2019-05-18 03:18:45 +0900

    organize if

--- jsqubits.d.ts
+++ jsqubits.d.ts
@@ -5,18 +5,18 @@ declare namespace jsqubits {
     interface QState {
       multiply(amount: number | Complex): QState;
       tensorProduct(qState: QState): QState;
-      x(targetBits: number | number[] | InternalJSQubitsStatic["ALL"] | BitsRange): QState;
-      X(targetBits: number): QState;
-      not(targetBits: number): QState;
-      y(targetBits: number): QState;
-      Y(targetBits: number): QState;
-      z(targetBits: number): QState;
-      Z(targetBits: number): QState;
-      hadamard(targetBits: number): QState;
-      s(targetBits: number): QState;
-      S(targetBits: number): QState;
-      t(targetBits: number): QState;
-      T(targetBits: number): QState;
+      x(targetBits: SingleQubitOperatorTargetQubits): QState;
+      X(targetBits: SingleQubitOperatorTargetQubits): QState;
+      not(targetBits: SingleQubitOperatorTargetQubits): QState;
+      y(targetBits: SingleQubitOperatorTargetQubits): QState;
+      Y(targetBits: SingleQubitOperatorTargetQubits): QState;
+      z(targetBits: SingleQubitOperatorTargetQubits): QState;
+      Z(targetBits: SingleQubitOperatorTargetQubits): QState;
+      hadamard(targetBits: SingleQubitOperatorTargetQubits): QState;
+      s(targetBits: SingleQubitOperatorTargetQubits): QState;
+      S(targetBits: SingleQubitOperatorTargetQubits): QState;
+      t(targetBits: SingleQubitOperatorTargetQubits): QState;
+      T(targetBits: SingleQubitOperatorTargetQubits): QState;
       cnot(controlBits: number, targetBits: number): QState;
       measure(bits: number | number[] | jsqubits.ALL | BitsRange): Measurement;
 
@@ -26,7 +26,6 @@ declare namespace jsqubits {
       add(qState: QState): QState;
       subtract(qState: QState): QState;
       normalize(): QState;
-
     }
 
     export type ALL = "ALL";
@@ -84,12 +83,9 @@ interface ComplexStatic {
   new (real: number, imaginary: number): jsqubits.jsqubits.Complex;
 }
 
-/*
-interface MeasurementStatic {
-  new (numBits: number, result: number, newState: jsqubits.jsqubits.QState): jsqubits.jsqubits.Measurement;
-}
-*/
 type MeasurementStatic = new (numBits: number, result: number, newState: jsqubits.jsqubits.QState) => jsqubits.jsqubits.Measurement;
+
+type SingleQubitOperatorTargetQubits = number | number[] | InternalJSQubitsStatic["ALL"] | BitsRange;
 
 interface BitsRange {
   from: number;
